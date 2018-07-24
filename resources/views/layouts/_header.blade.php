91bda4b79c911678cb0ff4101f8371eb7de4e53b
403228075 <403228075@qq.com>
2018-07-24 11:23:04 +0800

    用户界面 - 商品收藏

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,5 +1,16 @@
 <nav class="navbar navbar-default navbar-static-top">
     <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Laravel Shop
+            </a>
+        </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <ul class="nav navbar-nav">
             </ul>
