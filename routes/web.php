8d6b11ce2bac0d8d3af47c4f4709bf028bde814d
403228075 <403228075@qq.com>
2018-07-24 17:17:25 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,9 @@ Route::group(['middleware' => 'auth'], function() {
 
         Route::post('cart', 'CartController@add')->name('cart.add');
 
+        Route::get('cart', 'CartController@index')->name('cart.index');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
     });
 
 });
